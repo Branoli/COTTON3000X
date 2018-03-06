358e29b1300a4c698f8fe573d9efd3202e2f28f7
Branoli <lar.pasha.chenko@gmail.com>
2018-03-06 14:03:18 +0500

    Изменил вход, левое меню

--- app/src/main/java/com/example/cuser/cotton3000x/MainActivityUi.kt
+++ app/src/main/java/com/example/cuser/cotton3000x/MainActivityUi.kt
@@ -1,21 +1,41 @@
 package com.example.cuser.cotton3000x
 
-import com.example.cuser.cotton3000x.splach.SplashActivity
+import android.view.View
+import android.widget.EditText
+import com.example.cuser.cotton3000x.drawerlayoutANKO.DrawerActivity
 import org.jetbrains.anko.*
 
 class MainActivityUi : AnkoComponent<MainActivity> {
+    private lateinit var loginEdit : EditText
+    private lateinit var passwordEdit : EditText
+    private lateinit var butIn : Unit
+
     override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
         verticalLayout {
             horizontalPadding = dip(50)
-            val loginEdit = editText {
+            loginEdit = editText {
                 topPadding = dip(200)
                 hint = "Login"
             }
-            val passwordEdit = editText {
+            passwordEdit = editText {
                 hint = "Password"
             }
-            button("Войти").setOnClickListener{
-                startActivity<SplashActivity>()
+            val progBar = progressBar {
+                visibility = View.GONE
+            }
+            butIn = button("Войти").setOnClickListener{
+                loginEdit.visibility = View.INVISIBLE
+                passwordEdit.visibility = View.INVISIBLE
+                it.visibility = View.INVISIBLE
+                progBar.visibility = View.VISIBLE
+                doAsync {
+                    Thread.sleep(2000)
+                    uiThread {
+                        startActivity<DrawerActivity>()
+                    }
+                }
+
+                //startActivity<SplashActivity>()
             }
         }
     }
---
+++ app/src/main/java/com/example/cuser/cotton3000x/authorizationTask.kt
@@ -0,0 +1,10 @@
+package com.example.cuser.cotton3000x
+
+import android.os.AsyncTask
+
+class authorizationTask() : AsyncTask<Void, Void, Void>() {
+    override fun doInBackground(vararg params: Void?): Void {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
