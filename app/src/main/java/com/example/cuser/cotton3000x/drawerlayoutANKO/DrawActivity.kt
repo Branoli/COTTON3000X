af21916cbfa67c14661ca1aee3e87e4d1581a9c4
Branoli <lar.pasha.chenko@gamil.com>
2018-03-07 12:52:08 +0500

    nav_view

--- app/src/main/java/com/example/cuser/cotton3000x/drawerlayoutANKO/DrawActivity.kt
+++ app/src/main/java/com/example/cuser/cotton3000x/drawerlayoutANKO/DrawActivity.kt
@@ -2,12 +2,16 @@ package com.example.cuser.cotton3000x.drawerlayoutANKO
 
 import android.content.Context
 import android.graphics.Color
+import android.support.v7.appcompat.R.attr.theme
+
 import android.view.Gravity
 import android.view.Menu
+import android.view.View
 import android.widget.TextView
 import com.example.cuser.cotton3000x.R
 import org.jetbrains.anko.*
 import org.jetbrains.anko.appcompat.v7.actionMenuItemView
+import org.jetbrains.anko.custom.style
 import org.jetbrains.anko.design.appBarLayout
 import org.jetbrains.anko.design.coordinatorLayout
 import org.jetbrains.anko.design.navigationView
@@ -23,19 +27,22 @@ class DrawActivity(context: Context) : _DrawerLayout(context) {
         coordinatorLayout{
             appBarLayout{
                 toolbar {
-                    backgroundColor = Color.WHITE
+                    id = R.id.toolbar
+                    backgroundColor = Color.RED
                     popupTheme = R.style.AppTheme_PopupOverlay
-                }.lparams(matchParent, R.attr.actionBarSize)
+                }.lparams(matchParent, dip(70))
             }.lparams(matchParent, wrapContent)
+
+            linearLayout{
+                textView("MyName") {
+                }.lparams(wrapContent, wrapContent)
+            }.lparams(matchParent, matchParent)
+
         }.lparams(matchParent, matchParent)
 
-        linearLayout{
-
-            textView("MyName") {
-            }.lparams(wrapContent, wrapContent)
-        }.lparams(matchParent, matchParent)
 
         navigationView {
+            id = R.id.nav_bar
             fitsSystemWindows = true
             backgroundColor = Color.RED
 
@@ -67,7 +74,5 @@ class DrawActivity(context: Context) : _DrawerLayout(context) {
             fitsSystemWindows = true
             gravity = Gravity.LEFT
         }
-
-        openDrawer(Gravity.START)
     }
 }
--- app/src/main/java/com/example/cuser/cotton3000x/drawerlayoutANKO/DrawerActivity.kt
+++ app/src/main/java/com/example/cuser/cotton3000x/drawerlayoutANKO/DrawerActivity.kt
@@ -4,22 +4,67 @@ import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import com.example.cuser.cotton3000x.R
 import org.jetbrains.anko.custom.customView
-import org.jetbrains.anko.setContentView
+import org.jetbrains.anko.findOptional
 
 class DrawerActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var draw : DrawActivity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val draw = customView<DrawActivity>{}
+        draw = customView{}
+
+        val toolbar = findOptional<Toolbar>(R.id.toolbar)
+
+        //setSupportActionBar(R.id.toolbar)
+
+        /*
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+                this, draw, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        draw.addDrawerListener(toggle)
+        toggle.syncState()
+
+        findOptional<NavigationView>(R.id.nav_bar)!!.setNavigationItemSelectedListener(this)
+        */
+
     }
+
+    override fun onBackPressed() {
+        if (draw.isDrawerOpen(GravityCompat.START)) {
+            draw.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_camera -> {
-                Toast.makeText(getApplicationContext(), "Вы выбрали камеру", Toast.LENGTH_SHORT).show();
+
             }
             R.id.nav_gallery -> {
 
